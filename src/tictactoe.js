c51f52dcd360d84c59f240b1e9403e71f956bb40
Ukeje Michgolden <michgoldennathan17@gmail.com>
2020-02-26 14:12:26 +0100

    [Update] Change display on js

--- src/tictactoe.js
+++ src/tictactoe.js
@@ -46,46 +46,53 @@ const checkPlay = (() => {
     const selectQuery = query => document.querySelector(query);
     const finalSubmitBut = selectQuery("input[type='submit']");
     const player1Submit = selectQuery(".play1sub");
-    selectQuery("form").onsubmit = e => e.preventDefault();
+    const form = selectQuery("form");
+    form.onsubmit = e => e.preventDefault();
     const player1Name = selectQuery("#player1");
     const player2Name = selectQuery("#player2");
     console.log(player2Name);
-    
+
     const checkVal = inputval => {
       if (inputval.length > 0) {
         return true;
       }
       return false;
     };
-    // const hideElem =
     const showRules = () => {
       selectQuery(".rules").setAttribute(
         "style",
-        "height: 290px; width: 350px; padding: 10px"
+        "height: 300px; width: 350px; padding: 10px"
       );
-      console.log('works');
-      
+      console.log("works");
     };
 
     player1Submit.onclick = e => {
       console.log(player1Name.value);
       if (checkVal(player1Name.value)) {
-
+        selectQuery(".player2").style.display = "block";
         e.target.parentNode.classList.add("slide-out");
       } else {
-      console.log('works');
         e.target.parentNode.classList.add("vibrate");
+        e.target.parentNode.onanimationend = e => {
+          e.classList.remove("vibrate");
+        };
       }
     };
 
     finalSubmitBut.onclick = e => {
       if (checkVal(player2Name.value)) {
         e.target.parentNode.classList.add("slide-out");
+        form.style.display = "none";
         showRules();
-        selectQuery('.info-name').style.display = 'none';
-        selectQuery('.board-wrapper').style.visibility = 'visible';
+        selectQuery(".info-name").style.display = "none";
+        selectQuery(".board-wrapper").style.visibility = "visible";
+        selectQuery(".rules-board-wrap").display = "flex";
+        selectQuery(".gameboard").style.transform = "translate3d(0px, 0, 0px)";
       } else {
         e.target.parentNode.classList.add("vibrate");
+        e.target.parentNode.onanimationend = e => {
+          e.classList.remove("vibrate");
+        };
       }
     };
   };
