d8a168a41852c5af6b10a5ce0c6dfa5985b860eb
Ukeje Michgolden <michgoldennathan17@gmail.com>
2020-03-02 08:16:30 +0100

    [WIP] Fix linting errors on js

--- src/tictactoe.js
+++ src/tictactoe.js
@@ -6,15 +6,13 @@ const selectQuery = query => document.querySelector(query);
 const cells = document.querySelectorAll('.box');
 
 const game = (() => {
-  const display = () => {
-    board.map((item, index) => {
-      const cell = cells[index];
-      cell.innerHTML = item;
-    });
-  };
+  const display = () => board.map((item, index) => {
+    const cell = cells[index];
+    cell.innerHTML = item;
+  });
 
   function storePlayers(player1, player2) {
-	 players = [player1, player2];
+    players = [player1, player2];
   }
 
   const checkBox = position => {
@@ -25,13 +23,63 @@ const game = (() => {
     return false;
   };
 
-  const placeMark = (position) => {
+  const setWinner = (pattern, winner) => {
+    pattern.map(() => {
+      let winnerName;
+      let loserName;
+      if (winner === 'Player1') {
+        [winnerName, loserName] = players;
+      } else {
+        [loserName, winnerName] = players;
+      }
+      selectQuery('.turns').innerHTML = `${winner}: ${winnerName} won!!!`;
+    });
+
+    board.forEach((_item, index) => {
+      if (!pattern.includes(index)) {
+        board[index] = ':(';
+      }
+    });
+    display();
+  };
+
+  const checkWin = () => {
+    const patterns = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [2, 4, 0],
+      [0, 4, 8],
+    ];
+    function setTie() {
+      selectQuery('.turns').innerHTML = 'No Winner:  ITS A TIE!!!';
+    }
+
+    return patterns.map(p => {
+      if (board[p[0]] === 'X' && board[p[1]] === 'X' && board[p[2]] === 'X') {
+        return setWinner(p, 'Player1');
+      }
+      if (board[p[0]] === 'O' && board[p[1]] === 'O' && board[p[2]] === 'O') {
+        return setWinner(p, 'Player2');
+      }
+      if (board.indexOf('...') === -1 && board.indexOf(':(') === -1) {
+        return setTie();
+      }
+    });
+  };
+
+  const placeMark = position => {
     if (checkBox(position)) {
-	    countX = board.filter(x => x === 'X').length;
-	    countO = board.filter(y => y === 'O').length;
-	    if (countX > countO) {
-	    board[position] = 'O';
-	    } else { board[position] = 'X'; }
+      const countX = board.filter(x => x === 'X').length;
+      const countO = board.filter(y => y === 'O').length;
+      if (countX > countO) {
+        board[position] = 'O';
+      } else {
+        board[position] = 'X';
+      }
     }
     display();
     checkWin();
@@ -48,53 +96,19 @@ const game = (() => {
     window.location.reload();
   };
 
-  const checkWin = () => {
-    patterns = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [2, 4, 0], [0, 4, 8]];
-    patterns.map((p, i) => {
-      if ((board[p[0]] == 'X' && board[p[1]] == 'X' && board[p[2]] == 'X')) {
-	    return setWinner(p, 'Player1');
-      } if ((board[p[0]] == 'O' && board[p[1]] == 'O' && board[p[2]] == 'O')) {
-        return setWinner(p, 'Player2');
-      }
-      if (board.indexOf('...') === -1 && board.indexOf(':(') === -1) {
-	   selectQuery('.turns').innerHTML = 'No Winner:  ITS A TIE!!!';
-      }
-    });
-  };
-
-  const setWinner = (pattern, winner) => {
-    console.log(pattern);
-    pattern.map((item) => {
-      // cells[item].outerHTML = `<div class="box" data-key="${item+1}"> <i class="em em-fire" aria-role="presentation" aria-label="FIRE"></i></div>`
-
-      let winnerName;
-      if (winner == 'Player1') {
-        winnerName = players[0];
-      } else {
-   	winnerName = players[1];
-      }
-
-	    selectQuery('.turns').innerHTML = `${winner}: ${winnerName} won!!!`;
-    });
-
-    board.forEach((item, index) => {
-	  if (!pattern.includes(index)) { board[index] = ':('; }
-    });
-    display();
-  };
-
   return {
-    display, checkBox, placeMark, gameLogic, checkWin, setWinner, storePlayers, reStart,
+    display,
+    checkBox,
+    placeMark,
+    gameLogic,
+    checkWin,
+    setWinner,
+    storePlayers,
+    reStart,
   };
 })();
 
-
 const checkPlay = (() => {
-  const playersandTurns = [
-    ['Player1', 'X'],
-    ['Player2', 'O'],
-  ];
-
   const getInputs = () => {
     const selectQuery = query => document.querySelector(query);
     const finalSubmitBut = selectQuery("input[type='submit']");
@@ -131,7 +145,7 @@ const checkPlay = (() => {
 
     finalSubmitBut.onclick = e => {
       if (checkVal(player2Name.value)) {
-      	game.storePlayers(player1Name.value, player2Name.value);
+        game.storePlayers(player1Name.value, player2Name.value);
         e.target.parentNode.classList.add('slide-out');
         form.style.display = 'none';
         showRules();
@@ -150,12 +164,11 @@ const checkPlay = (() => {
 
   const clickListen = () => {
     cells.forEach((cell, ind) => {
-      cell.onclick = e => {
+      cell.onclick = () => {
         game.placeMark(ind);
       };
     });
   };
-
 
   return { clickListen, getInputs };
 })();
@@ -163,6 +176,5 @@ const checkPlay = (() => {
 checkPlay.clickListen();
 checkPlay.getInputs();
 
-
-const reStartGame = document.querySelector('button.restart-game');
+const reStartGame = selectQuery('button.restart-game');
 reStartGame.addEventListener('click', game.reStart);
